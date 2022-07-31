f90d9d1aaf0fd27f35fc27039919d06f655d9fcc
Frank Santolin <frank@cadha.com>
2022-07-31 11:50:13 +0100

    improvements

--- preferences/src/main/java/com/manning/application/notification/preferences/controllers/NotificationPreferencesController.java
+++ preferences/src/main/java/com/manning/application/notification/preferences/controllers/NotificationPreferencesController.java
@@ -3,6 +3,8 @@ package com.manning.application.notification.preferences.controllers;
 import com.manning.application.notification.preferences.model.NotificationPreferencesRequest;
 import com.manning.application.notification.preferences.model.NotificationPreferencesResponse;
 import com.manning.application.notification.preferences.services.NotificationPreferencesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "Preferences API", description = "Manage customer notification preferences")
 @RestController
 @RequiredArgsConstructor
 public class NotificationPreferencesController {
 
     private final NotificationPreferencesService service;
 
+    @Operation(summary = "Get the notification preferences for a customer")
     @GetMapping(path = "/notification/preferences", produces = MediaType.APPLICATION_JSON_VALUE)
     public NotificationPreferencesResponse getPreferences(@Valid NotificationPreferencesRequest request) {
         return service.findNotificationPreferences(request);
