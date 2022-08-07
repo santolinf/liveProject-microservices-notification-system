29c57c1700334435929ebc84fbe6b2e70b5cafd1
Frank Santolin <frank@cadha.com>
2022-08-07 16:00:35 +0100

    part 2 milestone 4 implement service discovery

--- application/src/main/java/com/manning/application/notification/config/ServiceClientsConfig.java
+++ application/src/main/java/com/manning/application/notification/config/ServiceClientsConfig.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The OpenFeign client is auto-integrated with service discovery.
+ * Declare an interface with required methods for each client. Annotate the interface with @FeignClient
+ * that points to the target service using its discovery name.
+ */
 @Configuration
 @EnableFeignClients(basePackages = "com.manning.application.notification.integration")
 public class ServiceClientsConfig {
--- application/src/main/java/com/manning/application/notification/integration/TemplateFormatterClient.java
+++ application/src/main/java/com/manning/application/notification/integration/TemplateFormatterClient.java
@@ -6,10 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(
-        name = "templateFormatterClient",
-        url = "${com.manning.application.service.template-formatter.url}"
-)
+@FeignClient(name = "${com.manning.application.service.template-formatter.id}")
 public interface TemplateFormatterClient {
 
     @PostMapping("/api/notifications/templates")
