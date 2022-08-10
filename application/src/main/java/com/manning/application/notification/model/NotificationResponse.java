a3aa46ea6a6080bc6341199c9fe1ab2e8ef97352
Frank Santolin <frank@cadha.com>
2022-08-10 20:57:30 +0100

    part 4 milestone 1 configure the circuit breaker resiliency pattern

--- application/src/main/java/com/manning/application/notification/model/NotificationResponse.java
+++ application/src/main/java/com/manning/application/notification/model/NotificationResponse.java
@@ -1,10 +1,12 @@
 package com.manning.application.notification.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
 import java.io.Serializable;
 
+@Builder
 @Data
 @ToString
 public class NotificationResponse implements Serializable {
