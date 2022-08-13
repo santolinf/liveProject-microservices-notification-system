7e78009eed3916de0b04be080583ebf5b1692a27
Frank Santolin <frank@cadha.com>
2022-08-13 16:18:36 +0100

    part 4 milestone 3 configure the retry resiliency pattern

--- application/src/main/java/com/manning/application/notification/integration/TemplateFormatterClient.java
+++ application/src/main/java/com/manning/application/notification/integration/TemplateFormatterClient.java
@@ -3,6 +3,7 @@ package com.manning.application.notification.integration;
 import com.manning.application.notification.common.model.NotificationTemplateRequest;
 import com.manning.application.notification.common.model.NotificationTemplateResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,20 @@ import static com.manning.application.notification.common.model.RemoteResponseSt
  * message that the notification has not been sent.
  * As a fallback, we could try a different service, save the notification for processing later however we could not return a cached
  * response as notifications are generally unique.
+ * <br/><br/>
+ * Circuit breaker with Retry
+ * <br/><br/>
+ * Due to Spring's method based AOP mechanism, by default, the retry mechanism has lower priority
+ * to the circuit breaker aspect. Hence, <code>Retry  ( Circuit Breaker ( function ) )</code>
+ * <br/><br/>
+ * To change this, we need to add an aspect order in the properties file.
+ * <br/><br/>
+ * Also, by not having a <em>fallback</em> on the Retry we allow the Circuit Breaker to take over when the retries have
+ * failed.
  */
 @FeignClient(name = "${com.manning.application.service.template-formatter.id}")
 @CircuitBreaker(name = "template-formatter", fallbackMethod = "templateFormatterFallback")
+@Retry(name = "template-formatter")
 public interface TemplateFormatterClient {
 
     @PostMapping("/api/notifications/templates")
